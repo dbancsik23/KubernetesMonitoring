e006e240c71c4a75588b23f6f229320d96e9c36d
David Bancsik <dbancsik@gmail.com>
2022-10-27 00:39:31 +0300

    minor corrections

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+from sqlalchemy import exc
 import logging
 import os
 import datetime
@@ -19,7 +20,7 @@ class Product(db.Model):
     ProductName = db.Column(db.String(100), nullable=False)
     ProductCode = db.Column(db.String(100), unique=True, nullable=False)
     Price = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True),server_default=func.now())
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
         return f"{self.ProductName} - {self.ProductCode} - {self.Price} - {self.created_at}"
@@ -36,7 +37,7 @@ def hello():
         return "Please use our API Services! Path = 'api/v1/<service_name>'"
 
 
-@app.route("/api/v1/product", methods=["POST","GET"])
+@app.route("/api/v1/product", methods=["POST", "GET"])
 def addproduct():
     dt = datetime.datetime.now()
     timestamp = f'[{dt.strftime("%d/%m/%Y, %H:%M:%S")}]'
@@ -50,11 +51,11 @@ def addproduct():
                 db.session.add(newproduct)
                 db.session.commit()
                 app.logger.info(f"{timestamp} {newproduct}")
-            except:
+            except exc.SQLAlchemyError:
                 app.logger.error(f"{timestamp} {data['ProductCode']} already exists!")
-                return f"{timestamp} {data['ProductCode']} already exists! Please check and try again!", 400
+                return f"{data['ProductCode']} already exists! Please check and try again!", 400
             return "", 200
-        app.logger.error(f"Content-Type not supported!")
+        app.logger.error(f"{timestamp} Content-Type not supported!")
         return "Content-Type not supported!", 400
     if request.method == "GET":
         productcode = request.args.get('ProductCode')
@@ -69,13 +70,11 @@ def addproduct():
                         "Added": f"{check_product.created_at}"
                         }
                 return data, 200
-            except:
+            except AttributeError:
                 app.logger.error(f"{timestamp} NOT FOUND {productcode}")
                 return f"{productcode} Not found", 404
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='logs.log', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
-    app.run(host="0.0.0.0", debug=True, port=3251)
-
-#TODO add datetime to the logs!
+    logging.basicConfig(filename='logs.log', level=logging.DEBUG)
+    app.run(host="0.0.0.0", port=3251)
