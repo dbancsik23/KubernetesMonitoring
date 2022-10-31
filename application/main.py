509f1af98327bdb8df2a1ea782e38d2c61c2abaf
David Bancsik <dbancsik@gmail.com>
2022-10-31 19:54:54 +0200

    added pv and pvc

--- application/main.py
+++ application/main.py
@@ -9,7 +9,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+        'sqlite:///' + os.path.join('/mnt/data', 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
